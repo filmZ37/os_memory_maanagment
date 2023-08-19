891f99921d39e64b595458289292af4e12f69228
filmZ37 <phattharapolb@gmail.com>
2023-08-19 15:18:36 +0700

    add show address of pointer in heap

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void free(void *ptr);
 void call_recursive(int n){
     if( n < 3){
         int data = n;
-        printf("In stack address of data %d is at %p.\n",data,&data);
+        printf("In stack: address of data %d is at %p.\n",data,&data);
         call_recursive(n+1) ;
     }
 
@@ -17,8 +17,8 @@ int main(int argc,char** argv){
     int *ptr1,*ptr2, *ptr3 ;
 
     //stack address section
-    printf("In stack address argc is at %p.\n",&argc);
-    printf("In stack address argv is at %p.\n",&argv);
+    printf("In stack: address argc is at %p.\n",&argc);
+    printf("In stack: address argv is at %p.\n",&argv);
     call_recursive(0);
 
     //heap address section
@@ -26,6 +26,9 @@ int main(int argc,char** argv){
     ptr2 = (int*)malloc(8*sizeof(int));
     ptr3 = (int*)malloc(16*sizeof(int));
 
+    printf("In heap: starting address of *ptr1 is at %p.\n",ptr1);
+    printf("In heap: starting address of *ptr2 is at %p.\n",ptr2);
+    printf("In heap: starting address of *ptr3 is at %p.\n",ptr3);
     free(ptr1);
     free(ptr2);
     free(ptr3);
